65b58137a44d5d4483b07df3a6cb6ee8dde108bb
unknown <eddevjo@gmail.com>
2023-07-09 14:40:35 +0100

    section: About me

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <div className=" text-center  p-3 bg-[#6AFF79]">
         Hey! Welcome To My Portfolio
       </div>
-      <div className="  text-white flex flex-wrap justify-center items-center gap-4">
-        <div className=" mr-auto">
+      <div className="  text-white flex flex-wrap justify-center items-center gap-6">
+        <div className=" mr-auto flex flex-col gap-2">
           <span className=" text-3xl">Mohamed Baarar</span>
 
           <p className=" font-normal text-[14px] ">
