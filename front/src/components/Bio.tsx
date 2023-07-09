8dbdf5dd58d39d9cbdc35404458beb7692e8cd85
unknown <eddevjo@gmail.com>
2023-07-09 15:06:53 +0100

    fix: grid layout height

--- front/src/components/Bio.tsx
+++ front/src/components/Bio.tsx
@@ -4,19 +4,22 @@ import arrow from "../../public/arrow.svg";
 
 const Bio = () => {
   return (
-    <div className="flex flex-col gap-6">
-      <div className="text-secondary text-xl font-semibold ">#About me</div>
-      <p className="text-white text-[15px]">
-        &nbsp; My name is Mohamed Baarar, You can call me Moe, i’m someone who
-        is passionate about creating and building things, I find great joy in
-        exploring the vast and dynamic world of technology. From designing and
-        developing websites and apps, to video games development and exploring
-        the the vast world of data, I am constantly seeking out new challenges
-        and opportunities to expand my skills and knowledge.
-      </p>
-      <div className="  flex gap-4 justify-center items-center text-black text-[16px] self-center bg-secondary py-2 px-6">
-        <span>My portfolio</span>
-        <Image src={arrow} width={6} height={6} alt="arrow" />
+    <div className="flex flex-col gap-2 text-[15px]">
+      <div className="text-secondary text-xl font-semibold ">#Bio</div>
+      <div className="bio-item text-white flex gap-6">
+        <span className="font-bold">2000</span>
+        <span className="">Born in Morocco</span>
+      </div>
+      <div className="bio-item text-white flex gap-6">
+        <span className="font-bold">2021</span>
+        <span className="">
+          Associate Degree in software engineering (DUT) from the High School of
+          Technology (EST) in Agadir
+        </span>
+      </div>
+      <div className="bio-item text-white flex gap-6">
+        <span className="font-bold">2023</span>
+        <span className="">Bachelor degree in data science</span>
       </div>
     </div>
   );
--- front/src/components/Grid.tsx
+++ front/src/components/Grid.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const grid = () => {
-  return <div className="gridy absolute h-screen w-screen">grid</div>;
+  return <div className="gridy absolute h-[100%] w-screen">grid</div>;
 };
 
 export default grid;
