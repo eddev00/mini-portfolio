3d431f9c9cac2823d91702a2b5d427ae0e836670
unknown <eddevjo@gmail.com>
2023-07-10 06:40:08 +0100

    temp: added links to github projects

--- front/src/app/Projects/page.tsx
+++ front/src/app/Projects/page.tsx
@@ -27,22 +27,26 @@ const page = () => {
           </div>
           <div className="container flex flex-wrap gap-6 mx-auto justify-between mb-6">
             <ProjectItem
+              link="https://github.com/eddev00/cosmos-memory-webapp"
               image={project1}
               title="Cosmos Memory"
               paragraph="A react app that display data about the galaxy from NASA API"
             />
 
             <ProjectItem
+              link="https://github.com/eddev00/langchain_fsaa_v1"
               image={langchain}
               title="Conversational chatbot"
               paragraph=" Chatbot model built on top of LLM model to answer from private data"
             />
             <ProjectItem
+              link="https://github.com/eddev00/-Expense-Tracker-Application-MERN"
               image={expensetracker}
               title="Expense Tracker"
               paragraph="A react app that track your expenses"
             />
             <ProjectItem
+              link="https://github.com/eddev00/mini-portfolio"
               image={portfolio}
               title="Personal Portfolio"
               paragraph="My personal portfolio website built with Next.js and Tailwindcss"
@@ -54,13 +58,15 @@ const page = () => {
             <div className="text-center  p-3 bg-[#6AFF79] font-bold text-lg">
               University Projects
             </div>
-            <div className="container_items flex flex-wrap gap-6 breakpoint:mx-auto">
+            <div className="container flex flex-wrap gap-6 mx-auto justify-between mb-6">
               <ProjectItem
+                link="https://github.com/eddev00/mini-portfolio"
                 image={project2}
                 title="University schooling dashboard"
                 paragraph="web app that manage the schooling process of a university + a chatbot that answer students questions "
               />
               <ProjectItem
+                link="https://github.com/eddev00/mini-portfolio"
                 image={chatbot}
                 title="A chatbot for the university"
                 paragraph="A chatbot that answer students questions about the university"
--- front/src/components/ProjectItem.tsx
+++ front/src/components/ProjectItem.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
-const ProjectItem = ({ image, title, paragraph }: any) => {
+const ProjectItem = ({ image, title, paragraph, link }: any) => {
   return (
-    <div className="text-white flex flex-col items-center gap-2 breakpoint:w-[45%] ">
-      <div className="bg-black border border-secondary breakpoint:h-[180px]">
-        <div className=" h-full flex items-center">
-          <Image src={image} className=" w-[90%] mx-auto py-4 " alt="gif1" />
+    <Link href={link} target="_blank">
+      <div className="text-white flex flex-col items-center gap-2 breakpoint:w-[45%] ">
+        <div className="bg-black border border-secondary breakpoint:h-[180px]">
+          <div className=" h-full flex items-center">
+            <Image src={image} className=" w-[90%] mx-auto py-4 " alt="gif1" />
+          </div>
         </div>
+        <span className=" text-center capitalize ">{title}</span>
+        <span className=" text-center font-normal text-[14px]">
+          {paragraph}
+        </span>
       </div>
-      <span className=" text-center capitalize ">{title}</span>
-      <span className=" text-center font-normal text-[14px]">{paragraph}</span>
-    </div>
+    </Link>
   );
 };
 
